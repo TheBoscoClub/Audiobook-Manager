5d23c2f079ba6a0f8a46452f001b98823a390a3d
Bosco Robinson <gjbr@pm.me>
2026-04-07 09:20:12 -0500

    style: apply shfmt formatting to 7 shell scripts

--- lib/audiobook-config.sh
+++ lib/audiobook-config.sh
@@ -132,7 +132,7 @@ fi
 : "${AUDIOBOOKS_AUDIBLE_CMD:=${AUDIOBOOKS_AUDIBLE_VENV}/bin/audible}"
 
 # Cloudflare CDN cache purge (credentials from ~/.config/api-keys.env)
-: "${CF_ZONE_ID:=}"  # Set in /etc/audiobooks/audiobooks.conf or environment
+: "${CF_ZONE_ID:=}" # Set in /etc/audiobooks/audiobooks.conf or environment
 
 # Server settings
 : "${AUDIOBOOKS_API_PORT:=5001}"
